b72f934b10bc84e59dbf0896b4bd161dad2a00d9
shivam2736 <156469253+shivam2736@users.noreply.github.com>
2025-09-05 11:40:15 -0400

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,25 +1,11 @@
 fn main() {
-    let numbers = [1, 9, -2, 0, 23, 20, -7, 13, 37, 20, 56, -18, 20, 3];
-    let mut max: i32;
-    let mut min: i32;
-    let mut mean: f64;
+    let celsius_temp = 23.0;
+    let fahrenheit_temp = celsius_to_fahrenheit(celsius_temp);
 
-    max = numbers[0];
-    min = numbers[0];
-    mean = 0.0;
+    assert_eq!(fahrenheit_temp, 73.4);
+    println!("Test passed!");
+}
 
-    for &num in numbers.iter() {
-        if num > max {
-            max = num;
-        } else if num < min {
-            min = num;
-        }
-        mean += num as f64;
-    }
-    mean /= numbers.len() as f64;
-
-    assert_eq!(max, 56);
-    assert_eq!(min, -18);
-    assert_eq!(mean, 12.5);
-    println!("Tests passed!");
+fn celsius_to_fahrenheit(temp: f64) -> f64 {
+    temp * 1.8 + 32.0
 }
