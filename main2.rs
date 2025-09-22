dec67235d292cd0a9246225c25735c7221c79d84
shivam2736 <156469253+shivam2736@users.noreply.github.com>
2025-09-21 20:09:18 -0400

    Add files via upload

--- main2.rs
+++ main2.rs
@@ -1,9 +1,30 @@
-fn main() {
-    let mut stuff: (u8, f32, char) = (10, 3.14, 'x');
-    stuff.0 += 3;
-    let first_item = stuff.0;
-    println!("first_item is {}", first_item);
+struct Rectangle{
+    width: f64,
+    height: f64
+}
 
-    let (a, b, c) = stuff;
-    println!("b is {}", b);
+impl Rectangle {
+    fn get_area(&self) -> f64 {
+        self.width * self.height
+    }
+
+    fn scale(&mut self, scalar: f64) {
+        self.width *= scalar;
+        self.height *= scalar;
+    }
+
+    fn new(width: f64, height: f64) -> Rectangle {
+        Rectangle {
+            width,
+            height
+        }
+    }
+}
+
+fn main() {
+    let mut rect = Rectangle::new(1.2, 3.4);
+    assert_eq!(rect.get_area(), 4.08);
+    rect.scale(0.5);
+    assert_eq!(rect.get_area(), 1.02);
+    println!("Tests passed!");
 }
